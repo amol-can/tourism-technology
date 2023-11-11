75e82f42b3aa416bcab1e8f2cc30372113a8298c
Amol Patil <amol.patil@zooplus.com>
2023-11-11 18:00:07 +0100

    transction service and points

---
+++ src/main/java/com/tourism/tourismtechnology/service/TransactionService.java
@@ -0,0 +1,60 @@
+package com.tourism.tourismtechnology.service;
+
+import com.tourism.tourismtechnology.entity.Reward;
+import com.tourism.tourismtechnology.entity.Transaction;
+import com.tourism.tourismtechnology.repository.TransactionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.List;
+
+@Service
+public class TransactionService {
+
+    private final TransactionRepository transactionRepository;
+    private final RewardService rewardService;
+
+    @Autowired
+    public TransactionService(TransactionRepository transactionRepository, RewardService rewardService) {
+        this.transactionRepository = transactionRepository;
+        this.rewardService = rewardService;
+    }
+
+    public List<Transaction> getAllTransactions() {
+        return transactionRepository.findAll();
+    }
+
+    public Transaction getTransactionById(Long id) {
+        return transactionRepository.findById(id).orElse(null);
+    }
+
+    public List<Transaction> getTransactionsByClientId(Long id) {
+        return transactionRepository.findByClientId(id);
+    }
+
+    public List<Transaction> getTransactionsByBusinessId(Long id) {
+        return transactionRepository.findByBusinessId(id);
+    }
+
+    public Transaction createTransaction(Transaction transaction) {
+        Transaction savedTransaction = transactionRepository.save(transaction);
+
+        // Calculate reward points based on the transaction amount
+        int points = savedTransaction.getAmount().intValue();
+
+        // Create a new Reward object and set the points and transaction
+        Reward reward = new Reward();
+        reward.setPoints(points);
+        reward.setTransaction(savedTransaction);
+        reward.setDate(new Date());
+
+        rewardService.createReward(reward);
+
+        return savedTransaction;
+    }
+
+    public void deleteTransaction(Long id) {
+        transactionRepository.deleteById(id);
+    }
+}
