0887a84f803f13101a4e136e25550c1220ee51a2
Amol Patil <amol.patil@zooplus.com>
2023-11-12 09:05:47 +0100

    bugfix

--- src/main/java/com/tourism/tourismtechnology/service/DashboardService.java
+++ src/main/java/com/tourism/tourismtechnology/service/DashboardService.java
@@ -1,5 +1,6 @@
 package com.tourism.tourismtechnology.service;
 
+import com.tourism.tourismtechnology.entity.User;
 import com.tourism.tourismtechnology.model.Dashboard;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -9,17 +10,25 @@ public class DashboardService {
 
     private final PointService pointService;
     private final TransactionService transactionService;
+    private final AuthService authService;
 
     @Autowired
-    public DashboardService(PointService pointService, TransactionService transactionService) {
+    public DashboardService(PointService pointService, TransactionService transactionService, AuthService authService) {
         this.pointService = pointService;
         this.transactionService = transactionService;
+        this.authService = authService;
     }
 
     public Dashboard getDashboardConfiguration(Long userId) {
         Dashboard dashboard = new Dashboard();
         dashboard.setTotalPoints(pointService.getTotalPointsByUserId(userId).getPoints());
-        dashboard.setNrOfTransactions((long) transactionService.getTransactionsByBusinessId(userId).size());
+
+        User user = authService.getUserById(userId);
+        if (user.getRole() == User.ROLE.CUSTOMER) {
+            dashboard.setNrOfTransactions((long) transactionService.getTransactionsByClientId(userId).size());
+        } else {
+            dashboard.setNrOfTransactions((long) transactionService.getTransactionsByBusinessId(userId).size());
+        }
         return dashboard;
     }
 }
